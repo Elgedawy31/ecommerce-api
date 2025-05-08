21c169c38e449a21c22dd8efe015e0421aa714b2
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-05-08 21:02:10 +0300

    save changes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Users } from './interfaces/user.interface';
 import { ResponseShape } from 'src/interfaces/response.interface';
@@ -11,6 +17,10 @@ export class UsersController {
     @Query('page') page = 1,
     @Query('limit') limit = 10,
   ): Promise<ResponseShape> {
-    return this.UserServices.findAll(page, limit);
+    try {
+      return this.UserServices.findAll(page, limit);
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
   }
 }
