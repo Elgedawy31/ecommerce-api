a953e5eae2a7852c83b24a880d9c4b483d72a270
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-05-08 19:53:18 +0300

    save changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // لجعل ConfigModule متاحًا بشكل عام في جميع أنحاء المشروع
+      envFilePath: '.env', // تحديد مسار ملف البيئة
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
