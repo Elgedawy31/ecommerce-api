e2ed28b60eb324ba10e1154663fae1bab1b986d3
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-05-09 19:04:29 +0300

    save changes

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query } from "@nestjs/common";
 import { ProductsService } from "./products.service";
+import { CreateProductDto } from "./dto/create.product.dto";
+import { ResponseShape } from "src/interfaces/response.interface";
 
 @Controller('products')
 export class ProductsController{
@@ -7,5 +9,10 @@ export class ProductsController{
   @Get()
   getAll(@Query('page') page:number , @Query('limit') limit:number){
     return this.ProductsService.findAll(page , limit)
+  }
+
+  @Post()
+  create(@Body() body:CreateProductDto):Promise <ResponseShape | null>{
+    return this.ProductsService.createProduct(body)
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,24 +1,60 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { Model } from "mongoose";
-import { Product } from "./interfaces/products.interface";
-import { ResponseShape } from "src/interfaces/response.interface";
+import { Inject, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Product } from './interfaces/products.interface';
+import { ResponseShape } from 'src/interfaces/response.interface';
+import { CreateProductDto } from './dto/create.product.dto';
+import { UpdateProductDto } from './dto/update.product.dto';
 
 @Injectable()
-export class ProductsService{
-  constructor(@Inject('PRODUCTS_MODEL') private ProductModel:Model<Product>){}
-    async findAll(page: number = 1, limit: number = 10): Promise<ResponseShape> {
-      const skip = (page - 1) * limit;
-      const totalUsers = await this.ProductModel.countDocuments();
-      const users = await this.ProductModel.find().skip(skip).limit(limit).exec();
-      const totalPages = Math.ceil(totalUsers / limit) || 0
+export class ProductsService {
+  constructor(@Inject('PRODUCTS_MODEL') private ProductModel: Model<Product>) {}
+  async findAll(page: number = 1, limit: number = 10): Promise<ResponseShape> {
+    const skip = (page - 1) * limit;
+    const totalProducts = await this.ProductModel.countDocuments();
+    const Products = await this.ProductModel.find().skip(skip).limit(limit).exec();
+    const totalPages = Math.ceil(totalProducts / limit) || 0;
+    return {
+      page,
+      limit,
+      data: Products,
+      total: totalProducts,
+      totalPages,
+      success: true,
+    };
+  }
+
+  async createProduct(body: CreateProductDto): Promise<ResponseShape> {
+    const Product = new this.ProductModel(body);
+    const createdProduct = await Product.save();
+    return {
+      data: createdProduct,
+      message: 'Product created successfully',
+      success: true,
+    };
+  }
+    async updateProduct(id: string, body: UpdateProductDto): Promise<ResponseShape> {
+      const updatedProduct = await this.ProductModel.findByIdAndUpdate(id, body, {
+        new: true,
+      });
       return {
-        page,
-        limit,
-        data: users,
-        total: totalUsers,
-        totalPages,
+        data: updatedProduct,
+        message: 'product created successfully',
         success: true,
       };
     }
-  
+    async getProduct(id: string): Promise<ResponseShape> {
+      const Product = await this.ProductModel.findById(id);
+      return {
+        data: Product,
+        success: true,
+      };
+    }
+    async deleteProduct(id: string): Promise<ResponseShape> {
+      const Product = await this.ProductModel.findByIdAndDelete(id);
+      return {
+        data: Product,
+        message: 'Product deleted successfully',
+        success: true,
+      };
+    }
 }
