03a7334e4812eee4bf76f4bffd1d001396e0a999
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-05-09 18:02:47 +0300

    save changes

--- src/users/auth/auth.controller.ts
+++ src/users/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, HttpException, HttpStatus, Post, ValidationPipe 
 import { AuthService } from "./auth.service";
 import { ResponseShape } from "src/interfaces/response.interface";
 import { SignInDto } from "../dto/signin.dto";
+import { SignUpDtop } from "../dto/signup.dto";
 
 
 @Controller('auth')
@@ -14,6 +15,14 @@ export class  AuthController{
         const {email , password} = body
         try {
             return await this.AuthService.signIn(email , password)
+        } catch (error) {
+            throw new  HttpException(error , HttpStatus.BAD_REQUEST)
+        }
+    }
+    @Post('register')
+    async register(@Body(new ValidationPipe({whitelist:true})) body:SignUpDtop) : Promise<ResponseShape | null>{
+        try {
+            return await this.AuthService.signUp(body)
         } catch (error) {
             throw new  HttpException(error , HttpStatus.BAD_REQUEST)
         }
--- src/users/auth/auth.service.ts
+++ src/users/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { Users } from '../interfaces/user.interface';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { ResponseShape } from 'src/interfaces/response.interface';
+import { SignUpDtop } from '../dto/signup.dto';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +14,7 @@ export class AuthService {
   ) {}
 
   async signIn(email: string, password: string): Promise<ResponseShape | null> {
-    const user = await this.userModel.findOne({ email });
+    const user = await this.userModel.findOne({ email }).select('+password');
     if (!user) {
       throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
@@ -21,7 +22,6 @@ export class AuthService {
     if (!isPasswordValid) {
       throw new HttpException('Invalid password', HttpStatus.UNAUTHORIZED);
     }
-
     const token = await this.JwtService.signAsync(
       { id: user._id, email: user.email, role: user.role },
       { secret: process.env.JWT_SECRET },
@@ -30,6 +30,22 @@ export class AuthService {
     return {
       data: { user, token },
       message: 'user login successfully',
+      success: true,
+    };
+  }
+  async signUp(body: SignUpDtop): Promise<ResponseShape | null> {
+    const { password } = body;
+    const hashedPassword = await bcrypt.hash(password, 10); // Added salt rounds and await
+    const user = new this.userModel({ ...body, password: hashedPassword });
+    const createdUser = await user.save();
+    const token = await this.JwtService.signAsync(
+      { id: user._id, email: user.email, role: user.role },
+      { secret: process.env.JWT_SECRET },
+    );
+
+    return {
+      data: { user: createdUser, token },
+      message: 'user created successfully',
       success: true,
     };
   }
