62b936f7b2f2cb89c2bd3f3f5032ffa65a43fa26
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-05-09 14:45:40 +0300

    save changes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,15 +6,21 @@ import {
   HttpStatus,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Users } from './interfaces/user.interface';
 import { ResponseShape } from 'src/interfaces/response.interface';
 import { CreateUserDto } from './dto/create-user.dto';
+import { RolesDecorator } from 'src/guards/roles.decorator';
+import { UsersGuard } from 'src/guards/authentization.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly UserServices: UsersService) {}
+
+  @RolesDecorator(['admin', 'user'])
+  @UseGuards(UsersGuard)
   @Get()
   getUsers(
     @Query('page') page = 1,
@@ -26,19 +32,23 @@ export class UsersController {
       throw new HttpException(error, HttpStatus.BAD_REQUEST);
     }
   }
-
+  @RolesDecorator(['admin'])
+  @UseGuards(UsersGuard)
   @Post()
   async create(@Body() body: CreateUserDto): Promise<ResponseShape> {
-   try {
-     return await this.UserServices.createUser(body);
-   } catch (error) {
+    try {
+      return await this.UserServices.createUser(body);
+    } catch (error) {
       // Provide more specific error message
       const errorMessage = error.message || 'Failed to create user';
-      throw new HttpException({
-        message: errorMessage,
-        error: error.name || 'Bad Request',
-        statusCode: HttpStatus.BAD_REQUEST,
-      }, HttpStatus.BAD_REQUEST);
-   }
+      throw new HttpException(
+        {
+          message: errorMessage,
+          error: error.name || 'Bad Request',
+          statusCode: HttpStatus.BAD_REQUEST,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
