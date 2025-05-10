2c71a1d292466d85efb65a4097e40933edf7d609
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-05-10 22:31:16 +0300

    save changes

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { CartService } from "./cart.service";
 import { CartController } from "./cart.controller";
+import { CartsProviders } from "./cart.provider";
+import { DatabaseModule } from "src/database/database.module";
 
 @Module({
-imports:[] ,
+imports:[DatabaseModule] ,
 controllers:[CartController] ,
-providers:[CartService] ,
-exports:[]
+providers:[CartService , ...CartsProviders] ,
+exports:[...CartsProviders]
 })
 
 export class CartModule{}
